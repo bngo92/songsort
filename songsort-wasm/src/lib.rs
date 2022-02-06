ed720c8fe67fb206f2618919ca60026f9df9206d
Bryan Ngo <bngo92@gmail.com>
2022-02-05 22:37:51 -0800

    Add login page and demo mode

--- songsort-wasm/src/lib.rs
+++ songsort-wasm/src/lib.rs
@@ -64,12 +64,36 @@ pub async fn run() -> Result<(), JsValue> {
         let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
         let resp: Response = resp_value.dyn_into()?;
         if resp.status() == 401 {
-            window.alert_with_message("Please refresh")?;
+            window.alert_with_message("Please contact bngo92@gmail.com for support")?;
         } else {
             switch_pages(state, Page::Home).await?;
         }
     } else {
-        window.alert_with_message("unimplemented")?;
+        let a = Closure::wrap(Box::new(move || {
+            let window = web_sys::window().expect("no global `window` exists");
+            let location = window.location();
+            location.set_href(&format!("https://accounts.spotify.com/authorize?client_id=ee3d1b4f8d80477ea48743a511ef3018&redirect_uri={}&response_type=code", location.origin().unwrap().as_str())).unwrap();
+        }) as Box<dyn FnMut()>);
+        document
+            .get_element_by_id("login")
+            .ok_or_else(|| JsValue::from("login element missing"))?
+            .dyn_into::<HtmlButtonElement>()?
+            .set_onclick(Some(a.as_ref().unchecked_ref()));
+        a.forget();
+        let state_ref = Rc::clone(&state);
+        let a = Closure::wrap(Box::new(move || {
+            let state = Rc::clone(&state_ref);
+            state.borrow_mut().auth = String::from("demo");
+            wasm_bindgen_futures::spawn_local(async {
+                switch_pages(state, Page::Home).await.unwrap();
+            });
+        }) as Box<dyn FnMut()>);
+        document
+            .get_element_by_id("demo")
+            .ok_or_else(|| JsValue::from("demo element missing"))?
+            .dyn_into::<HtmlButtonElement>()?
+            .set_onclick(Some(a.as_ref().unchecked_ref()));
+        a.forget();
     };
     Ok(())
 }
@@ -103,7 +127,11 @@ async fn switch_pages(state: Rc<RefCell<State>>, next_page: Page) -> Result<(), 
             navbar.children().item(1).unwrap().remove();
             borrowed_state.queued_scores.clear();
         }
-        Page::Login => {}
+        Page::Login => {
+            if let Some(child) = main.first_element_child() {
+                child.remove();
+            }
+        }
     }
     drop(borrowed_state);
     match next_page {
@@ -181,7 +209,7 @@ async fn generate_home_page(state: &Rc<RefCell<State>>) -> Result<Element, JsVal
         .dyn_into::<HtmlInputElement>()?;
     input.set_type("text");
     input.set_id("input");
-    input.set_value("https://open.spotify.com/playlist/5jPjYAdQO0MgzHdwSmYPNZ?si=05d659645f2d4781");
+    input.set_value("https://open.spotify.com/playlist/37i9dQZF1DX49jUV2NfGku?si=379bbc586c78450a");
     input.set_class_name("col-7");
     form_row.append_child(&input)?;
     let import = document
@@ -274,10 +302,16 @@ async fn generate_home_page(state: &Rc<RefCell<State>>) -> Result<Element, JsVal
                 .await
                 .unwrap();
             let resp: Response = resp_value.dyn_into().unwrap();
-            if resp.status() == 201 {
-                load_playlists(state).await.unwrap();
+            match resp.status() {
+                405 => {
+                    demo_alert(&window).unwrap();
+                }
+                201 => {
+                    load_playlists(state).await.unwrap();
+                }
+                // TODO: error handling
+                _ => {}
             }
-            // TODO: error handling
         })
     }) as Box<dyn FnMut()>);
     import.set_onclick(Some(a.as_ref().unchecked_ref()));
@@ -446,10 +480,20 @@ async fn load_playlists(state: Rc<RefCell<State>>) -> Result<(), JsValue> {
                     let window = web_sys::window().expect("no global `window` exists");
                     let url = format!("/api/playlists/{}", id);
                     let request = query(&url, "DELETE", &state.borrow().auth).unwrap();
-                    JsFuture::from(window.fetch_with_request(&request))
+                    let resp_value = JsFuture::from(window.fetch_with_request(&request))
                         .await
                         .unwrap();
-                    load_playlists(state).await.unwrap();
+                    let resp: Response = resp_value.dyn_into().unwrap();
+                    match resp.status() {
+                        405 => {
+                            demo_alert(&window).unwrap();
+                        }
+                        201 => {
+                            load_playlists(state).await.unwrap();
+                        }
+                        // TODO: error handling
+                        _ => {}
+                    }
                 })
             }) as Box<dyn FnMut()>);
             button.set_onclick(Some(a.as_ref().unchecked_ref()));
@@ -466,6 +510,10 @@ async fn load_playlists(state: Rc<RefCell<State>>) -> Result<(), JsValue> {
         .await
         .unwrap();
     let resp: Response = resp_value.dyn_into().unwrap();
+    if resp.status() == 405 {
+        web_sys::console::log_1(&JsValue::from("Not supported in demo"));
+        return Ok(());
+    }
     let json = JsFuture::from(resp.json()?).await?;
     let playlists: Playlists = json.into_serde().unwrap();
     while let Some(child) = playlists_element.first_element_child() {
@@ -504,10 +552,16 @@ async fn load_playlists(state: Rc<RefCell<State>>) -> Result<(), JsValue> {
                     .await
                     .unwrap();
                 let resp: Response = resp_value.dyn_into().unwrap();
-                if resp.status() == 201 {
-                    load_playlists(state).await.unwrap();
+                match resp.status() {
+                    405 => {
+                        demo_alert(&window).unwrap();
+                    }
+                    201 => {
+                        load_playlists(state).await.unwrap();
+                    }
+                    // TODO: error handling
+                    _ => {}
                 }
-                // TODO: error handling
             })
         }) as Box<dyn FnMut()>);
         button.set_onclick(Some(a.as_ref().unchecked_ref()));
@@ -769,6 +823,10 @@ fn query(url: &str, method: &str, auth: &str) -> Result<Request, JsValue> {
         .headers()
         .set("Authorization", &format!("Basic {}", auth))?;
     Ok(request)
+}
+
+fn demo_alert(window: &Window) -> Result<(), JsValue> {
+    window.alert_with_message("Not supported in demo")
 }
 
 fn create_th(document: &Document, class: &str, text: &str) -> Result<Element, JsValue> {
